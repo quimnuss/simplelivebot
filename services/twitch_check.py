77c3b6cbaecb3adc6b70115355eb7c6b0600d089
quimnuss <lluna.nova@gmail.com>
2022-07-09 22:57:30 +0200

    twitchio attempt

--- services/twitch_check.py
+++ services/twitch_check.py
@@ -1,35 +1,36 @@
-from pprint import pprint
-from twitchAPI import Twitch, EventSub
-
 import os
+
+import twitchio
+from twitchio.ext import eventsub, commands
+
 from dotenv import load_dotenv
 load_dotenv()
 
-# this will be called whenever someone follows the target channel
-async def on_follow(data: dict):
-    pprint(data)
-
 TARGET_USERNAME = os.getenv('TWITCH_TARGET_USERNAME')
 WEBHOOK_URL = os.getenv('TWITCH_WEBHOOK_URL')
+CALLBACK_URL = os.getenv('TWITCH_CALLBACK_URL')
 APP_ID = os.getenv('TWITCH_APP_ID')
 APP_SECRET = os.getenv('TWITCH_APP_SECRET')
 
-twitch = Twitch(APP_ID, APP_SECRET)
-twitch.authenticate_app([])
 
-uid = twitch.get_users(logins=[TARGET_USERNAME])
-user_id = uid['data'][0]['id']
-# basic setup, will run on port 8080 and a reverse proxy takes care of the https and certificate
-hook = EventSub(WEBHOOK_URL, APP_ID, 8080, twitch)
-# unsubscribe from all to get a clean slate
-hook.unsubscribe_all()
-# start client
-hook.start()
-print('subscribing to hooks:')
-hook.listen_channel_follow(user_id, on_follow)
+TWITCH_CHANNEL_ID = os.getenv('TWITCH_CHANNEL_ID')
 
-try:
-    input('press Enter to shut down...')
-finally:
-    hook.stop()
-print('done')
+
+esbot = commands.Bot.from_client_credentials(client_id=APP_ID,
+                                         client_secret=APP_SECRET)
+esclient = eventsub.EventSubClient(esbot,
+                                   webhook_secret=APP_SECRET,
+                                   callback_route=CALLBACK_URL)
+# when subscribing (you can only await inside coroutines)
+
+eventsub_client.subscribe_channel_subscriptions(TWITCH_CHANNEL_ID)
+
+@esbot.event()
+async def eventsub_notification_subscription(payload: eventsub.ChannelSubscribeData):
+    print('Received event!')
+    channel = esbot.get_channel('channel')
+    await channel.send(f'{payload.data.user.name} followed woohoo!')
+
+esbot.loop.create_task(eventsub_client.listen(port=4000))
+esbot.loop.create_task(esbot.start())
+esbot.loop.run_forever()
